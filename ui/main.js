6f75f148e054ce9cae2212ce2cef0435c30dd0d7
chandrahassan <30906837+chandrahassan@users.noreply.github.com>
2017-08-16 20:38:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,8 +17,7 @@ button.onclick = function(){
 
 };
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function (){
     var request = new XMLHttpRequest();
@@ -36,6 +35,8 @@ submit.onclick = function (){
         }
     }    
     };
+    var nameInput = document.getElementById('name');
+var name = nameInput.value;
     request.open('GET','http://hharanchassan.imad.hasura-app.io/submit-name?name=' + name,true);
     request.send(null);
 };
