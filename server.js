1634dd5dd0de1cacaa4dd79ca6363a0321f18649
chandrahassan <30906837+chandrahassan@users.noreply.github.com>
2017-08-28 19:56:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -119,9 +119,15 @@ app.get('/articles/:articleName',function(req,res) {
   pool.query("SELECT * FROM article WHERE title ="+req.params.articleName,function(err,result){
    if(err){
        res.status(500).send(err.toString());
+   }else{
+       if(result.rows.length === 0){
+           res.status(404).send('article not found');
+       }else{
+           var articleData = result.rows[0];
+           res.send(createTemplate(articleData));
+       }
    }   
   });
-  res.send(createTemplate(articleData));
 });
 
 app.get('/ui/style.css', function (req, res) {
