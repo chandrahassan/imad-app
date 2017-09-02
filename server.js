98c9de6a4d11a81c9e296bf01719436662a429f3
chandrahassan <30906837+chandrahassan@users.noreply.github.com>
2017-09-02 20:16:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
-
+var crypto = require('crypto');
 var config = {
     user:'hharanchassan',
     database:'hharanchassan',
@@ -53,6 +53,15 @@ function createTemplate (data){
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+function hash(input){
+    var hashed = crypto.pbkdf2Sync(input, salt);
+}
+app.get('/hash/:input',function(req,res){
+    var hashedString = hash(request.params.input);
+    res.send(hashedString);
+});
+
+
 
 var pool = new Pool(config);
 app.get('/test-db',function(req,res){
