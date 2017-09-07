79cdec5cb4fa9be3c3dab3fd6cdddda7dc05870f
chandrahassan <30906837+chandrahassan@users.noreply.github.com>
2017-09-07 19:54:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,8 +83,14 @@ app.post('/login',function(req,res){
     if(err){
         res.status(500).send(err.toString());
     }else{
-         res.send('User successfully created:' + username);
-         }    
+        if(result.rows.length===0){
+            req.send(403).send('username/password is invalid');
+        }else{
+            var dbString = result.rows[0].password;
+            var salt = dbString.split('$')[2];
+          res.send('User successfully created:' + username);
+         }
+    }
     }); 
 });
 var pool = new Pool(config);
