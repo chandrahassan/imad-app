d1dc0fc195af633efc8797f7fb3c0af557c4b10a
chandrahassan <30906837+chandrahassan@users.noreply.github.com>
2017-09-11 18:53:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,14 +9,12 @@ submit.onclick = function (){
     request.onreadystatechange = function(){
     if(request.readyState=== XMLHttpRequest.DONE){
         if(request.status === 200){
-            var names = request.responseText;
-            names = JSON.parse(names);
-            var list = '';
-            for(var i=0;i<names.length;i++){
-            list += '<li>' + names[i] + '</li>';
-  }
-  var ul = document.getElementById('namelist');
-  ul.innerHTML = list;
+            console.log('user logged in');
+            alert('logged in successfully');
+        }else if(request.status === 403){
+            alert('username/password is incorrect');
+        }else if(request.status === 500){
+            alert('something went wrong on the server');    
         }
     }    
     };
